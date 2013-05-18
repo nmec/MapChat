0b6a3e4081d44aceaddae1c635ec9e7be1a84e14
Or Barmatz <barmatzor@gmail.com>
2013-05-18 15:59:05 +0100

    added geolocation binding refactoring

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,9 +1,57 @@
-App = Ember.Application.create();
-App.ApplicationController = Ember.Controller.extend({});
+App = Ember.Application.create({});
+App.ApplicationController = Ember.Controller.extend({
+	init: function(){
+		var _this = this;
+		
+		getGeoLocation();
+		initMap();
+		
+		function initMap(){
+			$(function(){
+				_this.map = new GMaps({
+					div: '#map-canvas',
+					lat: _this.get('lat'),
+					lng: _this.get('lng'),
+					zoom: 4,
+					mapTypeControl: false
+				});
+				_this.addObserver('lat', _this, 'geoPositionChangeHandler');
+				_this.addObserver('lng', _this, 'geoPositionChangeHandler');
+			});
+		}
+		
+		function getGeoLocation(){
+			GMaps.geolocate({
+				success: function(position) {
+					var coords = position.coords;
+					_this.set('lat', coords.latitude);
+					_this.set('lng', coords.longitude);
+				},
+				error: function(error) {
+					alert('Geolocation failed: '+error.message);
+				},
+				not_supported: function() {
+					alert("Your browser does not support geolocation");
+				}
+			});
+		}
+	},
+	geoPositionChangeHandler: function(){
+		this.map.setCenter(this.get('lat'), this.get('lng'));
+	},
+	map: null,
+	lat: 0,
+	lng: 0
+});
 App.IndexController = Ember.Controller.extend({
 	loggedIn: false,
+	name: null,
 	loginHandler: function(name){
-		this.toggleProperty('loggedIn');
+		if(name) {
+			this.toggleProperty('loggedIn');
+		} else {
+			alert('ENTER A NAME!');
+		}	
 	},
 	submitHandler: function(){
 		this.message;
@@ -23,16 +71,8 @@ App.FriendsController = Ember.ArrayController.extend({
 App.LoginController = Ember.Controller.extend({
 	needs: 'index',
 	loginHandler: function(){
-		this.get('controllers.index').loginHandler(this.name);
+		var index = this.get('controllers.index');
+		index.loginHandler(this.name);
+		index.set('name', this.name);
 	}
-});
-
-$(function(){
-	new GMaps({
-		div: '#map-canvas',
-		lat: 48.166085,
-		lng: 14.501953,
-		zoom: 4,
-		mapTypeControl: false
-	});
 });
